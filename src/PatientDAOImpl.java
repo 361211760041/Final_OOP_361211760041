6de9304083f1358958651563bebac1a28f672927
mouy <mouy0810781674@gmail.com>
2020-03-26 20:13:29 +0700

    Final

---
+++ src/PatientDAOImpl.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class PatientDAOImpl implements PatientDAO {
+    @Override
+    public List<Patient> getAllPatient() {
+        return null;
+    }
+
+    @Override
+    public void addPatient(Patient newPat) {
+
+    }
+
+    @Override
+    public void updatePatient(Patient Pat) {
+
+    }
+
+    @Override
+    public void deletePatient(int id) {
+
+    }
+
+    @Override
+    public Patient findPatient(int id) {
+        return null;
+    }
+}
